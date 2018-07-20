331326774c862f37b2ba557b05c6eb32c196d527
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-07-20 22:27:07 +0700

    Level 4 - Initialized 3 variables with 3 sentences

---
+++ manipulation-string/level-4/index.js
@@ -0,0 +1,7 @@
+let first_sentence = "This is my first sentence.";
+let second_sentence = "I am the next sentence.";
+let third_sentence = "Me too, a short sentence";
+
+console.log(first_sentence);
+console.log(second_sentence);
+console.log(third_sentence);
