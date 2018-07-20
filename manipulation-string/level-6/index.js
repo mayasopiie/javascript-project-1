0e1ae756a4d887aee9a6d7ab7da1458c4d198505
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-07-20 23:01:17 +0700

    Level 6 - Modified strings into upper case and lower case

---
+++ manipulation-string/level-6/index.js
@@ -0,0 +1,19 @@
+let first_sentence = "This is my first sentence.";
+let second_sentence = "I am the next sentence.";
+let third_sentence = "Me too, a short sentence.";
+
+let first_sentence_uppercase = first_sentence.toUpperCase();
+let first_sentence_lowercase = first_sentence.toLowerCase();
+let second_sentence_uppercase = second_sentence.toUpperCase();
+let second_sentence_lowercase = second_sentence.toLowerCase();
+let third_sentence_uppercase = third_sentence.toUpperCase();
+let third_sentence_lowercase = third_sentence.toLowerCase();
+
+console.log(first_sentence_uppercase);
+console.log(first_sentence_lowercase);
+console.log("---");
+console.log(second_sentence_uppercase);
+console.log(second_sentence_lowercase);
+console.log("---");
+console.log(third_sentence_uppercase);
+console.log(third_sentence_lowercase);
