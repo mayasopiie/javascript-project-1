e66c6b7bc332873a36899b8d663264ff66937ed0
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-07-20 15:04:50 +0700

    Level 1 - Transformed variables with arithmetic operations

---
+++ manipulation-number/level-1/index.js
@@ -0,0 +1,20 @@
+let a = 2;
+let b = 5;
+let c = 7;
+let d = 11;
+let e = 54;
+
+const phi = 3.14;
+const lambda = 30;
+
+b = b + 8;
+d = d - 2;
+a = a * phi;
+c = c / lambda;
+e = e % 7;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
