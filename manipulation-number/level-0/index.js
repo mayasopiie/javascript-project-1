2ea99bda41ceea4da08347c8720b96b6aef62b7c
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-07-20 14:46:00 +0700

    Level 0 - Add 4 let variables and 2 constant variables

---
+++ manipulation-number/level-0/index.js
@@ -0,0 +1,14 @@
+let a = 2;
+let b = 5;
+let c = 7;
+let d = 11;
+
+const phi = 3.14;
+const lambda = 30;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(phi);
+console.log(lambda);
