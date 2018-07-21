3f56ab14356579af0a8eb32f25f21f435f3429ba
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-07-21 23:25:07 +0700

    Level 14 - Prepared kind of base math operations

---
+++ calculator-math/level-14/index.js
@@ -0,0 +1,49 @@
+//--- Kind of Math Operations
+console.log("Kind of Math Operations:");
+
+//--- 1. ADDITION
+//--- Example:
+console.log("a. Addition");
+
+let addition = 2 + 2;
+console.log(`2 plus 2 is ${addition}`);
+
+//--- 2. SUBSTRACTION
+//--- Example:
+console.log("---");
+console.log("b. Substraction");
+
+let substraction = 8 - 5;
+console.log(`8 minus 5 is ${substraction}`);
+
+//--- 3. MULTIPLICATION
+//--- Example:
+console.log("---");
+console.log("c. Multiplication");
+
+let multiplication = 4 * 7;
+console.log(`4 times 7 is ${multiplication}`);
+
+//--- 4. DIVISION
+//--- Example:
+console.log("---");
+console.log("d. Division");
+
+let division = 42 / 7;
+console.log(`42 divide by 7 is ${division}`);
+
+//--- 5. MODULATION
+//--- Example:
+console.log("---");
+console.log("e. Modulation");
+
+let modulation = 20 % 3;
+console.log(`The modulation of 20 divide by 3 is ${modulation}`);
+
+//--- 6. SQUARE OF NUMBER
+//--- Example:
+console.log("---");
+console.log("f. Square of number");
+
+let square = 3 * 3;
+console.log(`3 squared is ${square}`);
