93ef99af8aded9ba60052127cbcac0c00b4a445a
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-07-21 20:22:09 +0700

    Level 8 - Compared bigger or smaller number with if...else

---
+++ comparison-number/level-8/index.js
@@ -0,0 +1,111 @@
+// Store different numbers into some variables
+let a = 8;
+let b = 3;
+let c = 15;
+let d = 11;
+
+// Create some if else statement blocks that tell if each number is bigger or smaller than our determined value
+
+// A block
+console.log("'a' Block");
+
+if (a > b){
+    console.log(`${a} is bigger than ${b}`);
+}
+else{
+    console.log(`${a} is smaller than ${b}`);
+}
+
+if (a > c){
+    console.log(`${a} is bigger than ${c}`);
+}
+else{
+    console.log(`${a} is smaller than ${c}`);
+}
+
+if (a > d){
+    console.log(`${a} is bigger than ${d}`);
+}
+else{
+    console.log(`${a} is smaller than ${d}`);
+}
+
+console.log("---");
+
+// B block
+console.log("'b' Block");
+
+if (b > a){
+    console.log(`${b} is bigger than ${a}`);
+}
+else{
+    console.log(`${b} is smaller than ${a}`);
+}
+
+if (b > c){
+    console.log(`${b} is bigger than ${c}`);
+}
+else{
+    console.log(`${b} is smaller than ${c}`);
+}
+
+if (b > d){
+    console.log(`${b} is bigger than ${d}`);
+}
+else{
+    console.log(`${b} is smaller than ${d}`);
+}
+
+console.log("---");
+
+// C block
+console.log("'c' Block");
+
+if (c > a){
+    console.log(`${c} is bigger than ${a}`);
+}
+else{
+    console.log(`${c} is smaller than ${a}`);
+}
+
+if (c > b){
+    console.log(`${c} is bigger than ${b}`);
+}
+else{
+    console.log(`${c} is smaller than ${b}`);
+}
+
+if (c > d){
+    console.log(`${c} is bigger than ${d}`);
+}
+else{
+    console.log(`${c} is smaller than ${d}`);
+}
+
+console.log("---");
+
+// D block
+console.log("'d' Block");
+
+if (d > a){
+    console.log(`${d} is bigger than ${a}`);
+}
+else{
+    console.log(`${d} is smaller than ${a}`);
+}
+
+if (d > b){
+    console.log(`${d} is bigger than ${b}`);
+}
+else{
+    console.log(`${d} is smaller than ${b}`);
+}
+
+if (d > c){
+    console.log(`${d} is bigger than ${c}`);
+}
+else{
+    console.log(`${d} is smaller than ${c}`);
+}
+
+console.log("---");
