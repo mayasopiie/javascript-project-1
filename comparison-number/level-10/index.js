3ab2a987aea72970843aaa35f22cb90f3df5e2c6
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-07-21 21:09:41 +0700

    Level 10 - Compared numbers with ternary operator

---
+++ comparison-number/level-10/index.js
@@ -0,0 +1,75 @@
+// Store different numbers into some variables
+let a = 8;
+let b = 3;
+let c = 15;
+let d = 11;
+
+// Use ternary format to compare the numbers despite of if...else
+
+// A block
+console.log("'a' Block");
+
+a > b ?
+    console.log(`${a} is bigger than ${b}`) :
+    console.log(`${a} is smaller than ${b}`);
+
+a > c ? 
+    console.log(`${a} is bigger than ${c}`) :
+    console.log(`${a} is smaller than ${c}`);
+
+a > d ?
+    console.log(`${a} is bigger than ${d}`) :
+    console.log(`${a} is smaller than ${d}`);
+
+console.log("---");
+
+// B block
+console.log("'b' Block");
+
+b > a ?
+    console.log(`${b} is bigger than ${a}`) :
+    console.log(`${b} is smaller than ${a}`);
+
+b > c ?
+    console.log(`${b} is bigger than ${c}`) :
+    console.log(`${b} is smaller than ${c}`);
+
+b > d ?
+    console.log(`${b} is bigger than ${d}`) :
+    console.log(`${b} is smaller than ${d}`);
+
+console.log("---");
+
+// C block
+console.log("'c' Block");
+
+c > a ?
+    console.log(`${c} is bigger than ${a}`) :
+    console.log(`${c} is smaller than ${a}`);
+
+c > b ?
+    console.log(`${c} is bigger than ${b}`) :
+    console.log(`${c} is smaller than ${b}`);
+
+c > d ?
+    console.log(`${c} is bigger than ${d}`) :
+    console.log(`${c} is smaller than ${d}`);
+
+console.log("---");
+
+// D block
+console.log("'d' Block");
+
+d > a ?
+    console.log(`${d} is bigger than ${a}`) :
+    console.log(`${d} is smaller than ${a}`);
+
+d > b ?
+    console.log(`${d} is bigger than ${b}`) :
+    console.log(`${d} is smaller than ${b}`);
+
+d > c ?
+    console.log(`${d} is bigger than ${c}`) :
+    console.log(`${d} is smaller than ${c}`);
+
+console.log("---");
